c9a3998c31e17d7f10d871c99e5390a38555be98
Frederick <tayf2335@usherbrooke.ca>
2025-02-03 22:06:26 -0500

    Enlever la marde de trop

--- KickMyB-Server-main/src/main/java/org/kickmyb/server/task/MProgressEventRepository.java
+++
@@ -1,9 +0,0 @@
-package org.kickmyb.server.task;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-// Mostly used to have creation method, could use cascading
-
-@Repository
-public interface MProgressEventRepository extends PagingAndSortingRepository<MProgressEvent, Long> { }
--- KickMyB-Server-main/src/main/java/org/kickmyb/server/task/MTaskRepository.java
+++
@@ -1,7 +0,0 @@
-package org.kickmyb.server.task;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MTaskRepository extends PagingAndSortingRepository<MTask, Long> { }
